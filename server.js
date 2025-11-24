2e788e04027db2a895c845ce25c9d78b59302c0e
Tharunteja <pellakurutharunteja@gmail.com>
2025-11-25 03:05:34 +0530

    final

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://admin-page-steel-three.vercel.app/",
     credentials: true,
   })  
 );
